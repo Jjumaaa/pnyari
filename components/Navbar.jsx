4c276ea866ff33a23595b5b26f33c80515e35937
Juma <gerald.juma@student.moringaschool.com>
2026-07-15 16:15:43 +0300

    style: add initial navbar styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,20 @@
 export default function Navbar() {
   return (
-    <nav>
-      <h1>PCEA Nyari Church</h1>
+    <nav className="bg-blue-900 text-white p-5">
+      <div className="flex justify-between items-center">
 
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Ministries</li>
-        <li>Contact</li>
-      </ul>
+        <h1 className="text-2xl font-bold">
+          PCEA Nyari Church
+        </h1>
+
+        <ul className="flex gap-6">
+          <li>Home</li>
+          <li>About</li>
+          <li>Ministries</li>
+          <li>Contact</li>
+        </ul>
+
+      </div>
     </nav>
   );
 }
