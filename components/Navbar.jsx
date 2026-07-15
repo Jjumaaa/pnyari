b358d067d2fac1c53dd14329ae67d89ed3a511c9
Juma <gerald.juma@student.moringaschool.com>
2026-07-15 15:52:02 +0300

    feat: create reusable navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>PCEA Nyari Church</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Ministries</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
