0dbdfe5333f1f7b7ef7967d7d8b8c9ecd8e22d25
Juma <gerald.juma@student.moringaschool.com>
2026-07-15 21:39:07 +0300

    Add responsive mobile navigation menu

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,4 +1,8 @@
+"use client";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="bg-[var(--color-sanctuary-blue)] text-white">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
@@ -46,10 +50,60 @@ export default function Navbar() {
         </button>
 
         {/* Mobile Menu Button */}
-        <button className="text-3xl md:hidden">
-          ☰
+        <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-3xl md:hidden"
+        >{isOpen ? "✕" : "☰"}
         </button>
       </div>
+
+      {isOpen && (
+        <div className="border-t border-white/20 bg-[var(--color-sanctuary-blue)] md:hidden">
+          <ul className="flex flex-col px-6 py-4">
+            <li>
+              <a className="block py-3" href="#">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="block py-3" href="#">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="block py-3" href="#">
+                Ministries
+              </a>
+            </li>
+
+            <li>
+              <a className="block py-3" href="#">
+                Sermons
+              </a>
+            </li>
+
+            <li>
+              <a className="block py-3" href="#">
+                Events
+              </a>
+            </li>
+
+            <li>
+              <a className="block py-3" href="#">
+                Contact
+              </a>
+            </li>
+
+            <li className="pt-4">
+              <button className="w-full rounded-md bg-[var(--color-ember-crimson)] px-4 py-3">
+                New Here?
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
